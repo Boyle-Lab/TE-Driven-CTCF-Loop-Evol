df289d4909398983aff1b5b15159fe4a5719110b
adadiehl <adadiehl@umich.edu>
2020-03-02 15:22:31 -0500

    Added hadoop resultsults. Updated binomial enrichment and loop intersections to load data from there.

--- TE-CTCF-Enrichment/binomial/do_binomial_tests.R
+++ TE-CTCF-Enrichment/binomial/do_binomial_tests.R
@@ -1,12 +1,10 @@
 # Read in complete repeatMasker data
-all_repeats = read.table("../data/hadoop/repeatmasker_with_tss_dists.dat", stringsAsFactors=FALSE, header=FALSE)
-colnames(all_repeats) = c("id_rmsk", "chrom", "chromStart", "chromEnd", "name", "class", "family", "pctDiv", "species", "nearest_gene", "tss_dist")
+all_repeats = read.table("../data/hadoop/repeatmasker_with_tss_dists.dat", stringsAsFactors=FALSE, header=FALSE, col.names=c("id_rmsk", "chrom", "chromStart", "chromEnd", "name", "class", "family", "pctDiv", "species", "nearest_gene", "tss_dist"))
 all_repeats$tss_dist_bin = cut(all_repeats$tss_dist, breaks = c(-Inf,-10000,-2000,0,2000,10000,Inf), include.lowest = FALSE, labels=FALSE)
 rownames(all_repeats) = all_repeats$id_rmsk
 
 # Read in the CTCF-TE intersection data
-all_ctcf_repeats = read.table("ctcf_te_intersection.all.txt", stringsAsFactors=FALSE, header=FALSE)
-colnames(all_ctcf_repeats) = c("id_rmsk", "chrom", "chromStart_rmsk", "chromEnd_rmsk", "name_rmsk", "class_rmsk", "family_rmsk", "pctDiv_rmsk", "id_ctcf", "chromStart_ctcf", "chromEnd_ctcf", "signalValue_ctcf", "peak_ctcf", "factor", "species", "cell")
+all_ctcf_repeats = read.table("../../data/hadoop/results/ctcf_te_intersection.all.txt", stringsAsFactors=FALSE, header=FALSE, col.names=c("id_rmsk", "chrom", "chromStart_rmsk", "chromEnd_rmsk", "name_rmsk", "class_rmsk", "family_rmsk", "pctDiv_rmsk", "id_ctcf", "chromStart_ctcf", "chromEnd_ctcf", "signalValue_ctcf", "peak_ctcf", "factor", "species", "cell"))
 
 # Add TSS distance bins to CTCF-TE intersections
 library(sqldf)
