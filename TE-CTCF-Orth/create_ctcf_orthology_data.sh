015a5e3eaac32578d6def9a87b2ad993465566ec
adadiehl <adadiehl@umich.edu>
2021-06-29 11:08:22 -0400

    Replaced misseed filename placeholders to correct file names.

--- TE-CTCF-Orth/create_ctcf_orthology_data.sh
+++ TE-CTCF-Orth/create_ctcf_orthology_data.sh
@@ -18,17 +18,17 @@ awk -v N=$(tail -n 1 dat.hg | awk '{print $4}') 'BEGIN {N++}; {printf "%s\t%d\t%
 # liftOver chains were prepared as described in ../data/liftover/README.
 # Mouse data were mapped to human using the set of swapped human to mouse
 # chains to come as close as possible to reversible mapping between species.
-bnMapper.py dat.hg ../data/liftover/hg19.mm9.rbest.chain.gz -f narrowPeak -k -i narrowPeak > CTCF.hg19.merged.lifted.bed
-bnMapper.py dat.hg ../data/liftover/hg19.mm9.rbest.chain.gz -f narrowPeak -k -i narrowPeak > CTCF.hg19.merged.lifted.bed
+bnMapper.py CTCF.hg19.merged.narrowPeak ../data/liftover/hg19.mm9.rbest.chain.gz -f narrowPeak -k -i narrowPeak > CTCF.hg19.merged.lifted.bed
+bnMapper.py CTCF.mm9.merged.narrowPeak ../data/liftover/hg19.mm9.rbest.rev.chain.gz -f narrowPeak -k -i narrowPeak > CTCF.mm9.merged.lifted.bed
 
 # Merge mapped peaks with native peaks to get union sets
-bedtools merge -i <(cat hg19.merged.narrowPeak CTCF.mm9.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, $2+$10}' | bedtools sort) -c 4,5 -o collapse -delim ","  | awk '{psum = 0; N=split($5, A, ","); for (i=0; i<=N; i++) {psum += A[i]}; printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, (psum/N)-$2}' >  CTCF.hg19.mm9.union.bed
+bedtools merge -i <(cat CTCF.hg19.merged.narrowPeak CTCF.mm9.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, $2+$10}' | bedtools sort) -c 4,5 -o collapse -delim ","  | awk '{psum = 0; N=split($5, A, ","); for (i=0; i<=N; i++) {psum += A[i]}; printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, (psum/N)-$2}' >  CTCF.hg19.mm9.union.bed
 bedtools merge -i <(cat CTCF.mm9.merged.narrowPeak CTCF.hg19.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, $2+$10}' | bedtools sort) -c 4,5 -o collapse -delim "," | awk '{psum = 0; N=split($5, A, ","); for (i=0; i<=N; i++) {psum += A[i]}; printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, (psum/N)-$2}'  >  CTCF.mm9.hg19.union.bed
 
 # Label columns to indicate species-specific/shared occupancy
 # Human-referenced
-bedtools intersect -v -a dat.hg -b CTCF.mm9.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\t1\t0\n", $1, $2, $3, $4, $10}' > tmp
-bedtools intersect -v -b dat.hg -a CTCF.mm9.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\t0\t1\n", $1, $2, $3, $4, $10}' >> tmp
+bedtools intersect -v -a CTCF.hg19.merged.narrowPeak -b CTCF.mm9.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\t1\t0\n", $1, $2, $3, $4, $10}' > tmp
+bedtools intersect -v -b CTCF.hg19.merged.narrowPeak -a CTCF.mm9.merged.lifted.bed | awk '{printf "%s\t%d\t%d\t%d\t%d\t0\t1\n", $1, $2, $3, $4, $10}' >> tmp
 bedtools intersect -v -a CTCF.hg19.mm9.union.bed -b tmp | awk '{printf "%s\t%d\t%d\t%d\t%d\t1\t1\n", $1, $2, $3, $4, $5}' > tmp2
 cat tmp tmp2 > CTCF.hg19.mm9.union.labelled.bed
 # Mouse-referenced
@@ -69,7 +69,7 @@ rm tmp*
 
 # Mouse-referenced
 # Step 1
-awk '{printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, $10}' dat.hg > tmp1
+awk '{printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, $10}' CTCF.hg19.merged.narrowPeak > tmp1
 awk '{printf "%s\t%d\t%d\t%d\t%d\n", $1, $2, $3, $4, $10}' CTCF.hg19.merged.lifted.bed > tmp2
 # Step 2, as above: Run commands in get_species-specfic_sites.R
 /usr/bin/env Rscript --vanilla get_species-specific_sites.R
