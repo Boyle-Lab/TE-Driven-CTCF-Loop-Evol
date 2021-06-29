9012541b2c05ebd0a96e40ee8ffc681fd8656b13
adadiehl <adadiehl@umich.edu>
2021-06-29 11:36:50 -0400

    corrected call to wrong shell script for mapLoopLoci

--- Loop-Conservation/run_loop-cons_pipeline.sh
+++ Loop-Conservation/run_loop-cons_pipeline.sh
@@ -8,6 +8,6 @@
 # This sets the extension on _either side_ of the CTCF peak,
 # so the overall resolution is twice this (e.g., set to 5 for
 # 10kb resolution).
-/bin/bash do_mapLoopLoci.sh 5 10k ../data/hadoop/chia_pet.dat ../data/liftover
-/bin/bash do_mapLoopLoci.sh 10 20k ../data/hadoop/chia_pet.dat ../data/liftover
-/bin/bash do_mapLoopLoci.sh 25 50k ../data/hadoop/chia_pet.dat ../data/liftover
+/bin/bash do_mapLoopLoci_ext.sh 5 10k ../data/hadoop/chia_pet.dat ../data/liftover
+/bin/bash do_mapLoopLoci_ext.sh 10 20k ../data/hadoop/chia_pet.dat ../data/liftover
+/bin/bash do_mapLoopLoci_ext.sh 25 50k ../data/hadoop/chia_pet.dat ../data/liftover
